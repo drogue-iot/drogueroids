1666ca57362b46b1f135cd8da8c247db0a3fb1e6
Jens Reimann <jreimann@redhat.com>
2022-09-30 14:30:44 +0200

    feat: prepare for reconnect, allow others to consume events

--- plugins/ble/index.js
+++ plugins/ble/index.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The Drogue IoT Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 const ENV_SERVICE = "0000181a-0000-1000-8000-00805f9b34fb";
 const TEMP_CHAR = "00002a1f-0000-1000-8000-00805f9b34fb";
 
@@ -8,13 +24,23 @@ const DFU_SERVICE = "00001000-b0cd-11ec-871f-d45ddf138840";
 
 class BlePluginInstance {
 
+    #device;
     #state;
-    #logger;
+    #injector;
     #buttonScanner;
     #lastPresses;
+    #reconnectTimer;
+    #slide;
+
+    active = false;
 
     onButton = (button) => {
     };
+
+    /**
+     * Set to receive events on these callbacks, rather than controlling the presentation.
+     */
+    externalizeEvents;
 
     /**
      * Connect to device. Must be called from a user-guesture context.
@@ -33,10 +59,33 @@ class BlePluginInstance {
             ],
         })
             .then((device) => {
+                // only mark active after the user selected a device.
+                this.active = true;
                 console.log("Got device:", device);
+                this.#device = device;
                 device.addEventListener("gattserverdisconnected", () => this.#onDisconnected());
-                return device.gatt.connect();
+                return this.#reconnect();
             })
+            .catch((err) => {
+                console.log("Failed to setup connection", err);
+                this.#onDisconnected(err);
+            })
+        ;
+    }
+
+    disconnect() {
+        this.active = false;
+        if (this.#device) {
+            this.#device.gatt.disconnect();
+        }
+    }
+
+    async #reconnect() {
+        if (!this.active) {
+            return;
+        }
+
+        return await this.#device.gatt.connect()
             .then((server) => {
                 console.info("Server connected:", server);
                 return server.getPrimaryService(ENV_SERVICE)
@@ -75,31 +124,38 @@ class BlePluginInstance {
                             })
                     })
             })
-            .then((state) => this.#setState(state))
-            .catch((err) => {
-                console.log("Failed to setup connection", err);
-                this.#onDisconnected(err);
-            })
-        ;
+            .then((state) => this.#setState(state));
     }
 
     #onDisconnected(reason) {
         console.log("Disconnected", reason);
         this.#state = undefined;
-        if (this.#logger) {
-            window.clearInterval(this.#logger);
-            this.#logger = undefined;
+        if (this.#injector) {
+            window.clearInterval(this.#injector);
+            this.#injector = undefined;
         }
         if (this.#buttonScanner) {
             window.clearInterval(this.#buttonScanner);
             this.#buttonScanner = undefined;
         }
+
+        // trigger reconnect if we are still active
+        if (this.active) {
+            // trigger reconnect if none is pending
+            if (!this.#reconnectTimer) {
+                this.#reconnectTimer = window.setTimeout(() => {
+                    this.#reconnectTimer = null;
+                    return this.#reconnect();
+
+                }, 500);
+            }
+        }
     }
 
     #setState(state) {
         this.#state = state;
-        this.#logger = window.setInterval(() => {
-            this.#log();
+        this.#injector = window.setInterval(() => {
+            this.#inject();
         }, 1000);
         this.#buttonScanner = window.setInterval(() => {
             this.#scanButtons();
@@ -117,7 +173,7 @@ class BlePluginInstance {
         console.debug("Checking - last:", this.#lastPresses, "new:", presses);
 
         if (!this.#lastPresses) {
-            this.#lastPresses = presses;
+            this.#lastPresses = {a: 0, b: 0};
             return;
         }
 
@@ -125,19 +181,37 @@ class BlePluginInstance {
             // fire A
             this.#lastPresses.a = presses.a;
             console.info("Pressed A");
-            this.onButton?.('a');
+            this.#fireButtonEvent('a');
         }
         if (this.#lastPresses.b !== presses.b) {
             // fire B
             this.#lastPresses.b = presses.b;
             console.info("Pressed B");
-            this.onButton?.('b');
+            this.#fireButtonEvent('b');
         }
     }
 
-    #log() {
-        const presses = this.presses;
-        console.log(`State - temp: ${this.temperature}, a: ${presses.a}, b: ${presses.b}`);
+    #fireButtonEvent(button) {
+        if (this.externalizeEvents) {
+            this.externalizeEvents.onButton?.(button);
+        } else {
+            this.onButton?.(button);
+        }
+    }
+
+    #inject() {
+        console.debug(`State - temp: ${this.temperature}, a: ${this.presses?.a}, b: ${this.presses?.b}`);
+
+        const elements = document.querySelectorAll("[data-ble]");
+        for (const element of elements) {
+            switch (element.dataset.ble) {
+                case "temperature": {
+                    // noinspection JSPrimitiveTypeWrapperUsage
+                    element.innerText = this.temperature;
+                    break;
+                }
+            }
+        }
     }
 
     get temperature() {
@@ -161,6 +235,11 @@ class BlePluginInstance {
         };
     }
 
+    slideChanged(slide) {
+        this.#slide = slide;
+        // inject right now
+        this.#inject();
+    }
 }
 
 const BlePlugin = {
@@ -171,11 +250,13 @@ const BlePlugin = {
         instance.onButton = (button) => {
             console.log("Button pressed:", button);
             if (button === "a") {
-                deck.left();
+                deck.prev();
             } else if (button === "b") {
-                deck.right();
+                deck.next();
             }
         };
+
+        deck.getConfig().ble = instance;
 
         deck.addKeyBinding(
             {keyCode: 67, key: 'C', description: "Connect bluetooth"},
@@ -183,7 +264,12 @@ const BlePlugin = {
                 instance
                     .connect();
             }
-        )
+        );
+
+        deck.addEventListener("slidechanged", (event) => {
+            instance.slideChanged(event.currentSlide);
+        })
     },
 
 }
+
